fd155d2432605315ca66dfc7cb21451b3f9bfb4d
HAKAM CHEDO <152120181096@ogrenci.ogu.edu.tr>
2022-08-08 20:46:14 +0300

    All file

---
+++ Flappybird/Flappybird/Main.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Flappybird
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.ScoreText = new System.Windows.Forms.Label();
+            this.GameTime = new System.Windows.Forms.Timer(this.components);
+            this.playText = new System.Windows.Forms.Label();
+            this.Ground = new System.Windows.Forms.PictureBox();
+            this.PipeBottom = new System.Windows.Forms.PictureBox();
+            this.PipeTop = new System.Windows.Forms.PictureBox();
+            this.Flappybird = new System.Windows.Forms.PictureBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PipeBottom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PipeTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Flappybird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ScoreText
+            // 
+            this.ScoreText.AutoSize = true;
+            this.ScoreText.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreText.Location = new System.Drawing.Point(12, 12);
+            this.ScoreText.Name = "ScoreText";
+            this.ScoreText.Size = new System.Drawing.Size(117, 29);
+            this.ScoreText.TabIndex = 4;
+            this.ScoreText.Text = "Score : 0";
+            // 
+            // GameTime
+            // 
+            this.GameTime.Enabled = true;
+            this.GameTime.Interval = 20;
+            this.GameTime.Tick += new System.EventHandler(this.GameTime_Tick);
+            // 
+            // playText
+            // 
+            this.playText.AutoSize = true;
+            this.playText.BackColor = System.Drawing.Color.Transparent;
+            this.playText.Font = new System.Drawing.Font("Segoe UI Symbol", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playText.ForeColor = System.Drawing.Color.Navy;
+            this.playText.Location = new System.Drawing.Point(142, 309);
+            this.playText.Name = "playText";
+            this.playText.Size = new System.Drawing.Size(143, 17);
+            this.playText.TabIndex = 5;
+            this.playText.Text = "Press SPACE to play";
+            // 
+            // Ground
+            // 
+            this.Ground.AccessibleName = "Ground";
+            this.Ground.Image = global::Flappybird.Properties.Resources.ground;
+            this.Ground.Location = new System.Drawing.Point(-17, 553);
+            this.Ground.Name = "Ground";
+            this.Ground.Size = new System.Drawing.Size(592, 79);
+            this.Ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ground.TabIndex = 0;
+            this.Ground.TabStop = false;
+            // 
+            // PipeBottom
+            // 
+            this.PipeBottom.Image = global::Flappybird.Properties.Resources.pipe;
+            this.PipeBottom.Location = new System.Drawing.Point(308, 309);
+            this.PipeBottom.Name = "PipeBottom";
+            this.PipeBottom.Size = new System.Drawing.Size(100, 253);
+            this.PipeBottom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PipeBottom.TabIndex = 3;
+            this.PipeBottom.TabStop = false;
+            // 
+            // PipeTop
+            // 
+            this.PipeTop.Image = global::Flappybird.Properties.Resources.pipedown;
+            this.PipeTop.Location = new System.Drawing.Point(292, -11);
+            this.PipeTop.Name = "PipeTop";
+            this.PipeTop.Size = new System.Drawing.Size(100, 204);
+            this.PipeTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PipeTop.TabIndex = 2;
+            this.PipeTop.TabStop = false;
+            // 
+            // Flappybird
+            // 
+            this.Flappybird.AccessibleName = "Flappybird";
+            this.Flappybird.Image = global::Flappybird.Properties.Resources.bird;
+            this.Flappybird.Location = new System.Drawing.Point(68, 246);
+            this.Flappybird.Name = "Flappybird";
+            this.Flappybird.Size = new System.Drawing.Size(54, 45);
+            this.Flappybird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Flappybird.TabIndex = 1;
+            this.Flappybird.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AccessibleName = "Main";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(416, 615);
+            this.Controls.Add(this.playText);
+            this.Controls.Add(this.Ground);
+            this.Controls.Add(this.ScoreText);
+            this.Controls.Add(this.PipeBottom);
+            this.Controls.Add(this.PipeTop);
+            this.Controls.Add(this.Flappybird);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FlappyBird";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Main_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Main_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PipeBottom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PipeTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Flappybird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Ground;
+        private System.Windows.Forms.PictureBox Flappybird;
+        private System.Windows.Forms.PictureBox PipeTop;
+        private System.Windows.Forms.PictureBox PipeBottom;
+        private System.Windows.Forms.Label ScoreText;
+        private System.Windows.Forms.Timer GameTime;
+        private System.Windows.Forms.Label playText;
+        private System.Windows.Forms.BindingSource bindingSource1;
+    }
+}
+
---
+++ Flappybird/Flappybird/Main.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Flappybird
+{
+    public partial class Main : Form
+    {
+
+        int pipeSpeed = 5; 
+        int gravity = 15; 
+        int score = 0;
+        int time = 0;
+        int extratime = 0;
+        public Main()
+        {
+            InitializeComponent();
+            GameTime.Enabled = false;
+        }
+
+        private void GameTime_Tick(object sender, EventArgs e)
+        {
+            Flappybird.Top += gravity;
+            PipeBottom.Left -= pipeSpeed;
+            PipeTop.Left -= pipeSpeed;
+            ScoreText.Text = "Score : " + score;
+
+            if (PipeBottom.Left < -40)
+            {
+                PipeBottom.Left = 500;
+                score++;
+            }
+
+            if (PipeTop.Left < -40)
+            {
+                PipeTop.Left = 400;
+                score++;
+            }
+
+            if(Flappybird.Bounds.IntersectsWith(PipeTop.Bounds) || 
+               Flappybird.Bounds.IntersectsWith(PipeBottom.Bounds) ||
+               Flappybird.Bounds.IntersectsWith(Ground.Bounds))
+            {
+                SoundPlayer Sound = new SoundPlayer(@"D:\Flappybird\Flappybird\Flappybird\SoundEffect\GameOver.wav");
+                Sound.Play();
+                GameOver();
+            }
+            time++;
+            if(time == 500 + extratime)
+            {
+                pipeSpeed += 1;
+                extratime += 100;
+            }
+        }
+
+        private void Main_KeyDown(object sender, KeyEventArgs e)
+        {
+            GameTime.Enabled = true;
+            playText.Hide();
+            
+            if (e.KeyCode == Keys.Space)
+                gravity = -7;
+        }
+
+        private void Main_KeyUp(object sender, KeyEventArgs e)
+        {
+            SoundPlayer Sound = new SoundPlayer(@"D:\Flappybird\Flappybird\Flappybird\SoundEffect\Fly.wav");
+            Sound.Play();
+            if (e.KeyCode == Keys.Space)
+                gravity = 7;
+        }
+
+        private void GameOver()
+        {
+            GameTime.Stop();
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            DialogResult result = MessageBox.Show("Game Over!","Game Over", buttons);
+            if(result == DialogResult.OK)
+            {
+                this.Controls.Clear();
+                pipeSpeed = 5;
+                gravity = 15;
+                score = 0;
+                time = 0;
+                extratime = 0;
+                this.InitializeComponent();
+                GameTime.Enabled = false;
+            }
+        }
+    }
+}
